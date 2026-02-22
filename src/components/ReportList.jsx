4d353619ef2cbeefed25e76bae3857f7b83c48bf
chaithralakshmi.s <chaithralakshmi.s@simplify3x.com>
2026-02-22 16:02:07 +0530

    validate input field

--- src/components/ReportList.jsx
+++ src/components/ReportList.jsx
@@ -32,6 +32,7 @@ function ReportList({ handleReportSelection, addReport, reports }) {
     const [search, setSearch] = useState("");
     const COUNT = 12;
     const [show, setShow] = useState(true);
+    const [errors, setErrors] = useState({})
 
 
 
@@ -43,9 +44,59 @@ function ReportList({ handleReportSelection, addReport, reports }) {
     }, [reports, page, search]);
 
     const handleAddReport = () => {
+        if (!validateReport()) return;
         addReport(state.title, state.subTitle)
+        closeModal()
+    }
+
+    const closeModal = () => {
         dispatch({ type: "RESET" })
         setModal(false)
+        setErrors({})
+    }
+
+    const validateReport = () => {
+        const errors = {}
+        if (!state.title) {
+            errors.title = "Title is required"
+        }
+        else if (state.title.length < 3) {
+            errors.title = "Title must be at least 3 characters long"
+        }
+        if (!state.subTitle) {
+            errors.subTitle = "Sub Title is required"
+        }
+        else if (state.subTitle.length < 3) {
+            errors.subTitle = "Sub Title must be at least 3 characters long"
+        }
+        setErrors(errors)
+        return Object.keys(errors).length === 0
+    }
+    
+    const validateTitle = () =>{
+        const errors = {}
+         if (!state.title) {
+            errors.title = "Title is required"
+        }
+        else if (state.title.length < 3) {
+            errors.title = "Title must be at least 3 characters long"
+        }else{
+            errors.title = ""
+        }
+        setErrors((prev) => ({ ...prev, ...errors }))
+    }
+
+     const validateSubTitle = () =>{
+        const errors = {}
+         if (!state.subTitle) {
+            errors.subTitle = "Sub Title is required"
+        }
+        else if (state.subTitle.length < 3) {
+            errors.subTitle = "Sub Title must be at least 3 characters long"
+        }else{
+            errors.subTitle = ""
+        }
+        setErrors((prev) => ({ ...prev, ...errors }))
     }
 
 
@@ -59,7 +110,7 @@ function ReportList({ handleReportSelection, addReport, reports }) {
         <div className={`w-full md:w-[300px] bg-white flex flex-col rounded-sm p-2 ${show ? "h-[calc(100vh-215px)]" : "h-[50px]"}`} >
             <div className={`flex justify-between ${show ? "" : "p-2"}`}>
                 <h3 className="text-sm font-bold">Select Report Dashboard</h3>
-                
+
                 <div className="cursor-pointer md:hidden">
                     {show ? <FaAngleDown onClick={() => setShow(false)} /> : <FaAngleUp onClick={() => setShow(true)} />}
                 </div>
@@ -69,7 +120,7 @@ function ReportList({ handleReportSelection, addReport, reports }) {
                 <>
                     <hr className="text-gray mt-3" />
                     <div className="flex gap-2 mt-3">
-                        <input type="text" placeholder="Search" className="bg-gray-100 rounded-sm w-[80%] p-1" value={search} onChange={(e) => handleSearch(e.target.value)} />
+                        <input type="text" name="search" placeholder="Search" className="bg-gray-100 rounded-sm w-[80%] p-1" value={search} onChange={(e) => handleSearch(e.target.value)} />
                         <button className="bg-blue-800 text-white rounded-sm w-[20%] cursor-pointer" onClick={() => setModal(true)}>Add</button>
                     </div>
                     <div className="overflow-auto flex-1 min-h-0 mt-1">
@@ -77,7 +128,7 @@ function ReportList({ handleReportSelection, addReport, reports }) {
                             visibleReports.length > 0 ? visibleReports.map((report) => (
                                 <div key={report.id} className="flex gap-2 mt-3 justify-between rounded-sm bg-gray-100 px-2 py-1">
                                     <label htmlFor="">{report.title}</label>
-                                    <input type="checkbox" onChange={() => handleReportSelection(report)} />
+                                    <input type="checkbox" className="cursor-pointer" onChange={() => handleReportSelection(report)} />
                                 </div>
                             )) : <div className="text-center mt-3">No reports found</div>
                         }
@@ -88,23 +139,25 @@ function ReportList({ handleReportSelection, addReport, reports }) {
 
 
             {modal &&
-                <Modal setModal={() => setModal(false)} > <div className="flex justify-between">
+                <Modal setModal={() => closeModal()} > <div className="flex justify-between">
                     <h3 className='font-semibold text-sm'>Add Report Dashboard</h3>
-                    <button className='font-semibold cursor-pointer' onClick={() => setModal(false)}>x</button>
+                    <button className='font-semibold cursor-pointer text-md' onClick={() => closeModal()}>x</button>
                 </div>
                     <div className='mt-3'>
                         <div className='flex flex-col gap-1'>
-                            <label htmlFor="" className='text-sm'>Title</label>
-                            <input type="text" placeholder="Enter" className="border border-gray-200 rounded-sm  p-1" value={state.title} onChange={(e) => dispatch({ type: "SET_TITLE", payload: e.target.value })} />
+                            <label htmlFor="" className='font-semibold text-sm'>Title<span className="text-red-500">*</span></label>
+                            <input type="text" name="title" placeholder="Enter" className={`border ${errors.title ? "border-red-500" : "border-gray-300"} rounded-sm  p-1`} value={state.title} onChange={(e) => dispatch({ type: "SET_TITLE", payload: e.target.value })} onBlur={validateTitle} />
+                            {errors.title && <p className="text-red-500 text-xs">{errors.title}</p>}
                         </div>
                         <div className='flex flex-col gap-1 mt-2'>
-                            <label htmlFor="" className='text-sm'>Sub Title</label>
-                            <input type="text" placeholder="Enter" className="border border-gray-200 rounded-sm  p-1" value={state.subTitle} onChange={(e) => dispatch({ type: "SET_SUBTITLE", payload: e.target.value })} />
+                            <label htmlFor="" className='font-semibold text-sm'>Sub Title<span className="text-red-500">*</span></label>
+                            <input type="text" name="subTitle" placeholder="Enter" className={`border ${errors.subTitle ? "border-red-500" : "border-gray-300"} rounded-sm  p-1`} value={state.subTitle} onChange={(e) => dispatch({ type: "SET_SUBTITLE", payload: e.target.value })} onBlur={validateSubTitle} />
+                            {errors.subTitle && <p className="text-red-500 text-xs">{errors.subTitle}</p>}
                         </div>
                     </div>
                     <div className='mt-4 flex gap-2 justify-end'>
-                        <button className='bg-gray-200 text-black px-3 py-1 rounded-sm texy-sm cursor-pointer' onClick={() => setModal(false)}>Cancel</button>
-                        <button className='bg-blue-500 text-white px-5 py-1 rounded-sm texy-sm cursor-pointer' onClick={() => handleAddReport()}>Add</button>
+                        <button className='bg-gray-200 text-black px-4 py-1 rounded-sm font-semibold text-sm cursor-pointer' onClick={() => closeModal()}>Cancel</button>
+                        <button className='bg-blue-500 text-white px-4 py-1 rounded-sm font-semibold text-sm cursor-pointer' onClick={() => handleAddReport()}>Submit</button>
                     </div>
                 </Modal>}
         </div>
